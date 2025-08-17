2789bfdd5874ccaa56224ed6bd3a2b29bed644be
devpersi <109619062+devpersi@users.noreply.github.com>
2025-08-17 12:20:49 +0300

    add views, templates

--- medical/urls.py
+++ medical/urls.py
@@ -2,5 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /medical/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /medical/doctors/
+    path('doctors/', views.DoctorListView.as_view(), name='doctor_list'),
+    # ex: /medical/doctor/5/
+    path('doctor/<int:doctor_id>/', views.DoctorDetailView.as_view(), name='doctor_detail'),
+    # ex : /medical/patients/
+    path('patients/', views.PatientListView.as_view(), name='patient_list'),
+    # ex: /medical/patient/5/
+    path('patient/<int:patient_id>/', views.PatientDetailView.as_view(), name='patient_detail'),
+    # ex: /medical/questionnaires/
+    path('questionnaires/', views.QuestionnaireListView.as_view(), name='questionnaire_list'),
+    # ex: /medical/questionnaire/5/
+    path('questionnaire/<int:questionnaire_id>/', views.QuestionnaireDetailView.as_view(), name='questionnaire_detail'),
+    # ex: /medical/attempts/
+    path('attempts/', views.AttemptListView.as_view(), name='attempt_list'),
+    # ex: /medical/attempt/5/
+    path('attempt/<int:attempt_id>/', views.AttemptDetailView.as_view(), name='attempt_detail'),
 ]
--- medical/views.py
+++ medical/views.py
@@ -1,6 +1,73 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Doctor, Patient, Questionnaire, Attempt, QuestionCodeDescription, Answer
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the medical index.")
+class IndexView(TemplateView):
+    template_name = 'medical/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['doctor_list'] = Doctor.objects.order_by('-created_at')[:10]  # Get the last 10 doctors
+        context['patient_list'] = Patient.objects.order_by('-created_at')[:10]  # Get the last 10 patients
+        context['questionnaire_list'] = Questionnaire.objects.all()[:10]  # Get the last 10 questionnaires
+        context['attempt_list'] = Attempt.objects.order_by('-created_at')[:10]  # Get the last 10 attempts
+        context['year'] = 2025
+        return context
+
+class DoctorListView(ListView):
+    model = Doctor
+    template_name = 'medical/doctors.html'
+    context_object_name = 'doctor_list'
+    ordering = ['last_name']
+
+class DoctorDetailView(DetailView):
+    model = Doctor
+    template_name = 'medical/doctor.html'
+    context_object_name = 'doctor'
+    pk_url_kwarg = 'doctor_id'
+    
+class PatientListView(ListView):
+    model = Patient
+    template_name = 'medical/patients.html'
+    context_object_name = 'patient_list'
+    ordering = ['last_name']
+
+class PatientDetailView(DetailView):
+    model = Patient
+    template_name = 'medical/patient.html'
+    context_object_name = 'patient'
+    pk_url_kwarg = 'patient_id'
+    
+class QuestionnaireListView(ListView):
+    model = Questionnaire
+    template_name = 'medical/questionnaires.html'
+    context_object_name = 'questionnaire_list'
+    ordering = ['title']
+
+class QuestionnaireDetailView(DetailView):
+    model = Questionnaire
+    template_name = 'medical/questionnaire.html'
+    context_object_name = 'questionnaire'
+    pk_url_kwarg = 'questionnaire_id'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['question_codes'] = QuestionCodeDescription.objects.all()
+        return context
+
+class AttemptListView(ListView):
+    model = Attempt
+    template_name = 'medical/attempts.html'
+    context_object_name = 'attempt_list'
+    ordering = ['-created_at']
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patient_list'] = Patient.objects.all()
+        return context
+
+class AttemptDetailView(DetailView):
+    model = Attempt
+    template_name = 'medical/attempt.html'
+    context_object_name = 'attempt'
+    pk_url_kwarg = 'attempt_id'
